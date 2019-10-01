95928d7a0350973227d9829fb6c2eea2ba17b814
MaxRibeiroDMX <devmaxribeiro@gmail.com>
2019-10-01 20:19:00 -0300

    Sending the answer by using JSON than sa imple String

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const app = express();//Create a express application
 
 //Get defines the route ta receive the get request, and the action to perform for the request
 app.get('/',(req,res)=>{
-    return res.send('Hello World!');
+    return res.json({message : 'Hello World!'});//To work with rest api we have to send objects
 });
 
 app.listen(3333);//Defines in which port the server will be listen
