44dcf3fecff8e9df154e6880917cf74ae31be365
MaxRibeiroDMX <devmaxribeiro@gmail.com>
2019-10-02 11:37:15 -0300

    Fixed User not found in SpotController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
+
 const DashBoardController = require('./controllers/DashboardController');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const BookingController = require('./controllers/BookingController');
+
 const routes = express.Router();//Express router
 const upload = multer(uploadConfig);
 
@@ -11,6 +14,6 @@ routes.post('/sessions',SessionController.store);//Method to be acessed by the u
 routes.get('/spots', SpotController.index);//Get route to exibit the spots of application
 routes.post('/spots',upload.single('thumbnail'),SpotController.store);//Passing the path, the field of file and the controller to deal with that file
 routes.get('/dashboard',DashBoardController.show);
-
+routes.post('/spots/:spot_id/bookings',BookingController.store);//nested route
 //Exporting the routes
 module.exports = routes;
