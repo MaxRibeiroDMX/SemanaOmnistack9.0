0c6cc1186cb49f64d971c9140a016beb037c2af7
MaxRibeiroDMX <devmaxribeiro@gmail.com>
2019-10-01 21:03:03 -0300

    Testing PUT params

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,11 @@ const express = require('express');
 const app = express();//Create a express application
 //GET, POST. PUT, DELETE
 
-//req,query = Access query params   
+//req,query = Access query params to filter
+//req.params = Acess route params to edit and delete
 
-app.get('/users',(req,res)=>{
-    return res.json({idade : req.query.idade});//Req query get query paramns
+app.put('/users/:id',(req,res)=>{
+    return res.json({id : req.params.id});//Req query get query paramns
 });
 
 app.listen(3333);//Defines in which port the server will be listen
