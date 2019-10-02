c03d57b8442fcddd5a02efedde651397c80f9e88
MaxRibeiroDMX <devmaxribeiro@gmail.com>
2019-10-01 21:00:26 -0300

    Testing GET params using query

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,12 @@
 const express = require('express');
 
 const app = express();//Create a express application
+//GET, POST. PUT, DELETE
 
-app.post('/users',(req,res)=>{//Post route to send a post request using the insominia application
-    return res.json({message : 'Hello World!'});//To work with rest api we have to send objects
+//req,query = Access query params   
+
+app.get('/users',(req,res)=>{
+    return res.json({idade : req.query.idade});//Req query get query paramns
 });
 
 app.listen(3333);//Defines in which port the server will be listen
