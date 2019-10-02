85a24835933d230377d8308447a29e0571cb059c
MaxRibeiroDMX <devmaxribeiro@gmail.com>
2019-10-02 06:24:54 -0300

    Saving the info from form in postcontroller

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,7 +1,26 @@
+const Spot = require('../models/Spot');
+
 module.exports = {
     async store(req,res){
-        console.log(req.body);
-        console.log(req.file);
-        return res.json({message:"Ola Mundo!"});
+        //importing information from Spot model
+        const { filename } = req.file;
+        const {company, techs, price} = req.body;
+        const {user_id}=req.headers;
+
+        //User validation
+        const user = await User.findById(user_id);
+        if(!user){
+            return res.status(400).json({error:"Usuario não exist"});
+        }
+    
+        const spot = await Spot.create({
+            user:user_id,
+            thumbnail:filename,
+            company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price
+        })
+
+        return res.json(spot);
     }
 };
