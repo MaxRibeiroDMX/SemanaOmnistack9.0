94585a5d0fcb5b69408f09a16629094d18d7c3b2
MaxRibeiroDMX <devmaxribeiro@gmail.com>
2019-10-03 19:22:04 -0300

    Added route page, and changed the login page to proper page on application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,19 @@
-import React, {useState} from 'react';
-import api from './services/api';
+import React from 'react';
+
 import './App.css';
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
   //Use state returns a array
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event){
-    event.preventDefault();
-
-    const response = await api.post('/sessions', { email });
-    const {_id} = response.data;
-    localStorage.setItem('user', _id);
-  }
-
+ 
   return (
     <div className="container">
       <img src={logo} alt="airCnC"/>
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre  <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email"  placeholder="Seu melhor email" onChange={event=>setEmail(event.target.value)}  value={email}/>
-          <button type="submit" className="btn">ENTRAR</button>
-        </form>
+        <Routes/>
       </div>
     </div>
   );
